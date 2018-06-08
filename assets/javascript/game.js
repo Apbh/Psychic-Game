ce3cf31416d3b892f2969364ffc4649f03b3658d
Apbh <36648551+Apbh@users.noreply.github.com>
2018-06-07 23:09:45 -0300

    Add files via upload

---
+++ assets/javascript/game.js
@@ -0,0 +1,75 @@
+// Variables Defined
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var lettersChosen = [];
+var computerChoices = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var computerSelect = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+
+
+    //Functions Defined
+    function guessesLeft () {
+        document.getElementById("guesses").innerHTML = guesses;
+        
+    }
+    
+    function lettersUsed () {
+        document.getElementById("letters-chosen").innerHTML = lettersChosen;
+    }
+    
+    
+    //Function to reset the game being stored in a variable, to be called later
+    var reset = function () {
+
+        lettersChosen = [];
+        guesses = 9;
+        guessesLeft(0);
+        
+    }
+    
+    //onkeyup function will be executed when a player selects a letter key
+    
+    document.onkeyup = function(event) {
+        if (event.keyCode >= 65 && event.keyCode <=90){
+        var userSelect =event.key.toLowerCase(); 
+        guesses--;
+        lettersChosen.push(userSelect);
+        guessesLeft();
+        lettersUsed();
+
+        }
+        else {
+            alert("Please select a letter!")
+        } //to alert a player that they have selected a non alphabetic key
+        
+    
+    //These conditions need to be met, to increase wins or losses by 1
+    
+        if (userSelect === computerSelect) {
+            wins++;
+            document.querySelector("#wins").innerHTML = wins;
+            alert("You are a Psychic!");
+            reset()
+        }
+
+     else if (guesses === 0){
+            losses++;
+            document.querySelector("#losses").innerHTML = losses;
+            reset()
+        } 
+    }
+
+
+
+
+    
+
+
+
+
+
+    
+    
+    
+   
